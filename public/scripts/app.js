1af8be0b730f8da3f849af97eabde3d6cd22b80f
indraarianggi <indraarianggi@gmail.com>
2018-09-13 19:40:16 +0700

    conditional rendering in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,80 @@
 'use strict';
 
 // JSX - JavaScript XML
+var obj = {
+    title: 'Learn React from Udemy Course',
+    subtitle: 'By Andrew',
+    options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Oolloo World! This is JSX syntax from src/app.js'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        obj.title
+    ),
+    obj.subtitle && React.createElement(
+        'p',
+        null,
+        obj.subtitle
+    ),
+    obj.options.length > 1 ? 'Here are your options:' : 'No options!',
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var userName = 'Indra Arianggi';
+var userAge = 21;
+var userCity = 'Depok';
+var UserState = 'Indonesia';
+
+var user = {
+    name: 'Candra Kusuma',
+    age: 20,
+    location: 'Purworejo, Indonesia'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age,
+        ' yo'
+    ),
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
--- src/app.js
+++ src/app.js
@@ -1,6 +1,47 @@
 
 // JSX - JavaScript XML
-var template = <h1>Oolloo World! This is JSX syntax from src/app.js</h1>
+var obj = {
+    title   : 'Learn React from Udemy Course',
+    subtitle: 'By Andrew',
+    options : ['One', 'Two']
+}
+
+var template = (
+    <div>
+        <h1>{ obj.title }</h1>
+        { obj.subtitle && <p>{ obj.subtitle }</p> }
+        { (obj.options.length > 1) ? 'Here are your options:' : 'No options!' }
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var userName = 'Indra Arianggi';
+var userAge = 21;
+var userCity = 'Depok';
+var UserState = 'Indonesia';
+
+var user = {
+    name    : 'Candra Kusuma',
+    age     : 20,
+    location: 'Purworejo, Indonesia'
+};
+
+function getLocation(location) {
+    if(location) {
+        return <p>Location: { location }</p>;
+    }
+}
+
+var templateTwo = (
+    <div>
+        <h1>{ user.name ? user.name : 'Anonymous' }</h1>
+        { (user.age && user.age >= 18) && <p>Age: {user.age} yo</p> }
+        { getLocation(user.location) }
+    </div>
+);
 
 var appRoot = document.getElementById('app');
 
