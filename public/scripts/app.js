039dbbcafb9f17d9aba636e3c4a18fd568735d0b
indraarianggi <indraarianggi@gmail.com>
2018-09-16 15:53:44 +0700

    form, array

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,39 +4,85 @@
 var obj = {
     title: 'Learn React from Udemy Course',
     subtitle: 'By Andrew',
-    options: ['One', 'Two']
+    options: []
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        obj.title
-    ),
-    obj.subtitle && React.createElement(
-        'p',
-        null,
-        obj.subtitle
-    ),
-    obj.options.length > 1 ? 'Here are your options:' : 'No options!',
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+    if (option) {
+        obj.options.push(option);
+        e.target.elements.option.value = '';
+
+        // call render function
+        renderView();
+    }
+};
+
+var removeAll = function removeAll() {
+    obj.options = [];
+    renderView();
+};
+
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * obj.options.length);
+    var option = obj.options[randomNum];
+    alert(option);
+};
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderView = function renderView() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            obj.title
+        ),
+        obj.subtitle && React.createElement(
+            'p',
+            null,
+            obj.subtitle
+        ),
+        obj.options.length > 1 ? 'Here are your options:' : 'No options!',
+        React.createElement(
+            'ol',
+            null,
+            obj.options.map(function (opt) {
+                return React.createElement(
+                    'li',
+                    { key: opt },
+                    opt
+                );
+            })
+        ),
+        React.createElement(
+            'button',
+            { disabled: obj.options.length == 0, onClick: onMakeDecision },
+            'What should I do ?'
+        ),
+        React.createElement(
+            'button',
+            { onClick: removeAll },
+            'Remove All Options'
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+// Call render function
+renderView();
--- src/app.js
+++ src/app.js
@@ -3,21 +3,63 @@
 const obj = {
     title   : 'Learn React from Udemy Course',
     subtitle: 'By Andrew',
-    options : ['One', 'Two']
+    options : []
 }
 
-const template = (
-    <div>
-        <h1>{ obj.title }</h1>
-        { obj.subtitle && <p>{ obj.subtitle }</p> }
-        { (obj.options.length > 1) ? 'Here are your options:' : 'No options!' }
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value;
+    if(option) {
+        obj.options.push(option);
+        e.target.elements.option.value = '';
+        
+        // call render function
+        renderView();
+    }
+}
+
+const removeAll = () => {
+    obj.options = [];
+    renderView();
+}
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * obj.options.length);
+    const option = obj.options[randomNum];
+    alert(option);
+}
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+const renderView = () => {
+    const template = (
+        <div>
+            <h1>{ obj.title }</h1>
+            { obj.subtitle && <p>{ obj.subtitle }</p> }
+            { (obj.options.length > 1) ? 'Here are your options:' : 'No options!' }
+
+            <ol>
+                {
+                    obj.options.map((opt) => {
+                        return <li key={opt}>{opt}</li>
+                    })
+                }
+            </ol>
+
+            <button disabled={obj.options.length == 0} onClick={onMakeDecision}>What should I do ?</button>
+
+            <button onClick={removeAll}>Remove All Options</button>
+    
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+}
+
+// Call render function
+renderView();
