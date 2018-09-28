29dd1150532984818784715e866b1e3546e8ddbf
indraarianggi <indraarianggi@gmail.com>
2018-09-28 18:35:47 +0700

    setting up babel with webpack

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-// import './utils.js';
-import minus, { square, add } from './utils.js'
-import isSenior, { isAdult, canDrive } from './person';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running.');
-console.log(square(4));
-console.log(add(100, 1));
-console.log(minus(100, 45)); // reference to substract function in utils.js
-
-console.log(isAdult(10) ? 'Hello Man!' : 'Watsuup Kid?');
-console.log(canDrive(17) ? 'Have a nice drive' : 'Sorry, another time kids');
-console.log(isSenior(37) ? 'Masta' : 'Newbie');
+ const template = <p>Ooolllooooo</p>;
+ ReactDOM.render(template, document.getElementById('app'));
