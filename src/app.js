3f713a281a72c6c460e745dfd11207c33be11042
indraarianggi <indraarianggi@gmail.com>
2018-09-30 14:54:22 +0700

    webpack loader for styling (reguler css and sass)

--- src/app.js
+++ src/app.js
@@ -4,5 +4,8 @@ import ReactDOM from 'react-dom';
 // import component
 import IndecisionApp from './components/IndecisionApp';
 
+// import style
+import './styles/styles.scss';
+
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
